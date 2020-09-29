c38a20561468f60b6c85322dfa575095d36c7b79
mtdembele <60542984+mtdembele@users.noreply.github.com>
2020-09-29 17:36:53 -0400

    Update NotationStack.java

--- Assignment-2/NotationStack.java
+++ Assignment-2/NotationStack.java
@@ -1,28 +1,45 @@
-import java.util.ArrayList;
 
+import java.util.ArrayList;
+/**
+ * 
+ * @author Mohammad Dembele
+ *
+ * @param <T>
+ */
 public class NotationStack<T> implements StackInterface {
 
 	private ArrayList<T> elements;
-	private int head,top,maxSize;
+	private int maxSize=100;
 	
-	
+	/**size constructor
+	 * 
+	 * @param size
+	 */
 	public NotationStack(int size){
 		this.maxSize=size;
 		elements = new ArrayList<T>(size);
-		head = top  = 0;
+		
 	}
-	
+	/**
+	 * default constructor
+	 */
 	public NotationStack() {
-		this(10);
+		this(100);
 		
 	}
 	
-
-	public NotationStack(ArrayList<String> aList) {
+/**copies an arraylist for stack
+ * 
+ * @param aList array to be copied
+ */
+	public NotationStack(ArrayList<T> aList) {
 		this.elements=(ArrayList<T>) aList;
 		
 	}
-
+/**
+ * checks if stack is empty 
+ * @return true if stack is empty, false if string is not empty
+ */
 	@Override
 	public boolean isEmpty() {
 		if (elements.size()==0) 
@@ -30,7 +47,9 @@ public class NotationStack<T> implements StackInterface {
 		else 
 			return false; 
 	}
-
+/**checks if stack is full
+ * @return true if stack is full , false if stack is not full
+ */
 	@Override
 	public boolean isFull() {
 		if(elements.size()==maxSize)
@@ -38,7 +57,10 @@ public class NotationStack<T> implements StackInterface {
 		else 
 			return false;
 	}
-
+/** removes an object from the stack
+ * @return the object that is removed
+ * @throws StackUnderflowException
+ */
 	@Override
 	public Object pop() throws StackUnderflowException {
 		if(this.isEmpty()) {
@@ -50,7 +72,10 @@ public class NotationStack<T> implements StackInterface {
 			}
 		
 	}
-
+/**
+ * peeks top of stack\
+ * @return the object at top of stack, does not remove 
+ */
 	@Override
 	public Object top() throws StackUnderflowException {
 		if(this.isEmpty()) {
@@ -60,13 +85,19 @@ public class NotationStack<T> implements StackInterface {
 		return elements.get(elements.size()-1);
 			}
 	}
-
+/**
+ * checks size of stack
+ * @return size of stack
+ */
 	@Override
 	public int size() {
 		return elements.size();
 		
 	}
-
+/**adds object to stack
+ * @return true if object is successfully added
+ * @throws StackOverflowException
+ */
 	@Override
 	public boolean push(Object e) throws StackOverflowException {
 		if (this.isFull())
@@ -75,7 +106,10 @@ public class NotationStack<T> implements StackInterface {
 			elements.add((T) e);
 		return true;
 	}
-
+/**
+ * string representation of stack
+ * @return String separated by delimiter
+ */
 	@Override
 	public String toString(String delimiter) {
 		String result = "";
@@ -83,12 +117,15 @@ public class NotationStack<T> implements StackInterface {
 			if(elements.indexOf(ele)==elements.size()-1)
 				result+=ele;
 			else
-			result += ele + delimiter;
+			result += ele + delimiter; // Separated by delimiter
 			 
 		}
 		return result;
 	}
-	
+	/**
+	 *  string representation of queue
+	 *  @return String representation of queue
+	 */
 	@Override
 	public String toString() {
 		String result="";
