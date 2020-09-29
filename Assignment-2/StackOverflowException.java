07148491f335989fe9717e958ace41c30ca59b6a
mtdembele <60542984+mtdembele@users.noreply.github.com>
2020-09-29 17:40:24 -0400

    Update StackOverflowException.java

--- Assignment-2/StackOverflowException.java
+++ Assignment-2/StackOverflowException.java
@@ -1,6 +1,12 @@
-
-public class StackOverflowException extends Exception{
-
+/**
+ * 
+ * @author Mohammad Dembele
+ *
+ */
+public class StackOverflowException extends InvalidNotationFormatException{
+/**
+ * @return Exception message
+ */
 	public String getMessage() {
 		return "StackOverFlowException";
 	}
